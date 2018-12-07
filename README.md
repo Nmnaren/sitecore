61de2340aad14961bbf243698768c7eaa8561439
root <root@ip-172-31-23-214.us-west-2.compute.internal>
2018-12-07 10:31:55 +0000

    new file adding

---
+++ README.md
@@ -0,0 +1,20 @@
+#Sitecore.Demo
+
+This solution hosts the demo websites for Sitecore.
+
+* Habitat Legal - *Professional services vertical. Legal Services*
+* Habitat Finance - *Financial services vertical. Banking, Insurance, Investment*
+* Habitat Utility - *Utilities vertical. Power company*
+
+To install, set up a website running:
+* A Sitecore solution running the [Habitat](https://github.com/sitecore/habitat) framework.
+  * For more information about the Habitat project along with required dependencies, please check out the [Habitat Wiki](https://github.com/sitecore/habitat/wiki))
+* EXM
+* PXM
+* [xGenerator and EXM generator](https://github.com/Sitecore/xGenerator/releases)
+
+The code and scenarios in this repository is not generally supported and can be used as is.
+
+For more information:
+* If you are a partner or customer, please contact your local Sitecore sales or partner representative.  
+* For internal Sitecore support, please check the demo pages on [HUB](https://sitecore1.sharepoint.com/sites/productmarketing/PMC/SitePages/HUB/Demo%20tools.aspx)
---
+++ specs/Kakadu.TestScenarios/LegalWebsiteDemoScenarios/Infrastructure/TestsSetup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary1.Steps;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary1.Infrastructure
+{
+  [Binding]
+  public class TestsSetup
+  {
+    [BeforeStep]
+    public static void Timeout()
+    {
+#warning shitcode
+      Thread.Sleep(3000);
+    }
+
+    [BeforeScenario("UI")]
+    public static void Setup()
+    {
+
+      var firefoxProfile = new FirefoxProfile();
+      firefoxProfile.SetPreference("browser.download.folderList", 2);
+
+
+      firefoxProfile.SetPreference("browser.download.panel.shown", false);
+
+
+      firefoxProfile.SetPreference("pdfjs.disabled", true);
+      firefoxProfile.SetPreference("pdfjs.firstRun", false);
+
+      firefoxProfile.SetPreference("browser.download.dir", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\downloads" );
+      firefoxProfile.SetPreference("browser.helperApps.neverAsk.openFile", "application/pdf");
+      firefoxProfile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf");
+
+      var webDriver = (IWebDriver)new FirefoxDriver(firefoxProfile);
+
+      FeatureContext.Current.Set(webDriver); ;
+    }
+
+    [AfterScenario]
+    public void Cleanup()
+    {
+      ContextExtensions.CleanupPool.ForEach(CleanupExecute);
+    }
+
+    private static void CleanupExecute(TestCleanupAction payload)
+    {
+      if (payload.ActionType == ActionType.RemoveUser)
+      {
+        ContextExtensions.HelperService.DeleteUser(payload.GetPayload<string>());
+        return;
+      }
+      if (payload.ActionType == ActionType.CleanFieldValue)
+      {
+        var fieldPayload = payload.GetPayload<EditFieldPayload>();
+        ContextExtensions.UtfService.EditItem(fieldPayload.ItemIdOrPath, fieldPayload.FieldName, fieldPayload.FieldValue, Settings.UserName, Settings.Password, fieldPayload.Database);
+        return;
+      }
+
+      if (payload.ActionType == ActionType.DeleteItem)
+      {
+        var fieldPayload = payload.GetPayload<EditFieldPayload>();
+        ContextExtensions.UtfService.DeleteItem(fieldPayload.ItemIdOrPath, fieldPayload.Database, false);
+        return;
+      }
+
+      throw new NotSupportedException($"Action type '{payload.ActionType}' is not supported");
+    }
+
+
+    [AfterScenario]
+    public static void TeardownTest()
+    {
+      try
+      {
+        FeatureContext.Current.Get<IWebDriver>().Quit();
+      }
+      catch (Exception)
+      {
+        // Ignore errors if unable to close the browser
+      }
+    }
+
+    private static Settings Settings => new Settings();
+
+    public static WhenActionSteps WhenActionSteps =>new WhenActionSteps();
+
+
+
+
+  }
+}
